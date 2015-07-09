cdfabe4a51b51d2bd7f670e0f5b1b17b7beeb033
laniley <melanie.mende@me.com>
2015-07-09 20:05:45 +0200

    config

--- app/initializers/facebook.js
+++ app/initializers/facebook.js
@@ -1,10 +1,15 @@
 import FB from 'ember-cli-facebook-js-sdk/fb';
+import ENV from '../config/environment';
+
+export function initialize(/* container, application */) {
+  // application.inject('route', 'foo', 'service:foo');
+}
 
 export default {
   name: 'facebook',
   initialize: function() {
     return FB.init({
-      appId: '759505040738218',
+      appId: ENV.fb_app_id,
       version: 'v2.3',
       xfbml: true
     });
--- config/environment.js
+++ config/environment.js
@@ -1,14 +1,5 @@
 /* jshint node: true */
 
-// production
-// var app_id = 759505040738218;
-//
-// if(mode == 'debug')
-// {
-// 	// test
-// 	app_id = 761604617194927;
-// }
-
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'littlerocket',
@@ -25,6 +16,18 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+    },
+
+    // for the FB-SDK
+    contentSecurityPolicy: {
+      'default-src': "'none'",
+      'script-src': "'self' 'unsafe-inline' 'unsafe-eval' use.typekit.net connect.facebook.net facebook.com graph.facebook.com maps.googleapis.com maps.gstatic.com mt0.googleapis.com mt1.googleapis.com http://www.google-analytics.com/analytics.js",
+      'font-src': "'self' data: use.typekit.net fonts.gstatic.com",
+      'connect-src': "'self' localhost:8000",
+      'img-src': "'self' www.paypalobjects.com graph.facebook.com www.facebook.com p.typekit.net fbcdn-profile-a.akamaihd.net maps.gstatic.com mt0.googleapis.com mt1.googleapis.com maps.googleapis.com csi.gstatic.com mt.googleapis.com",
+      'style-src': "'self' 'unsafe-inline' use.typekit.net fonts.googleapis.com",
+      'frame-src': "s-static.ak.facebook.com static.ak.facebook.com www.facebook.com admin.appnext.com",
+      'report-uri': '/_/csp-reports'
     }
   };
 
@@ -34,6 +37,8 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
+
+    ENV.fb_app_id = 964330650255655;
   }
 
   if (environment === 'test') {
@@ -46,10 +51,12 @@ module.exports = function(environment) {
     ENV.APP.LOG_VIEW_LOOKUPS = false;
 
     ENV.APP.rootElement = '#ember-testing';
+
+    ENV.fb_app_id = 964330650255655;
   }
 
   if (environment === 'production') {
-
+    ENV.fb_app_id = 759505040738218;
   }
 
   return ENV;
