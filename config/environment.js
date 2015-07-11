fce8583e7a0bfa003e116fd8e4d00964a12dfb2b
laniley <melanie.mende@me.com>
2015-07-11 08:50:00 +0200

    styles

--- config/environment.js
+++ config/environment.js
@@ -34,8 +34,8 @@ module.exports = function(environment) {
   if (environment === 'development') {
     // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
-    // ENV.APP.LOG_TRANSITIONS = true;
-    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+    ENV.APP.LOG_TRANSITIONS = true;
+    ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
 
     ENV.fb_app_id = 964330650255655;
