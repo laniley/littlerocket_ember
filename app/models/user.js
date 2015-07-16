a6ab4c17d063bc1927b93d945bb26a63da2cfd3a
laniley <melanie.mende@me.com>
2015-07-16 13:44:44 +0200

    score added to user model

--- app/models/user.js
+++ app/models/user.js
@@ -4,5 +4,6 @@ export default DS.Model.extend({
   fb_id: DS.attr('string'),
   first_name: DS.attr('string'),
   last_name: DS.attr('string'),
-  img_url: DS.attr('string')
+  img_url: DS.attr('string'),
+  score: DS.attr('number', { defaultValue: 0 })
 });
