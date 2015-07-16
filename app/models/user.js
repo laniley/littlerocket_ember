8113e120131c6a3a0edc8c78ba0ae0d107efa801
laniley <melanie.mende@me.com>
2015-07-16 22:50:17 +0200

    quintus

--- app/models/user.js
+++ app/models/user.js
@@ -5,5 +5,6 @@ export default DS.Model.extend({
   first_name: DS.attr('string'),
   last_name: DS.attr('string'),
   img_url: DS.attr('string'),
-  score: DS.attr('number', { defaultValue: 0 })
+  score: DS.attr('number', { defaultValue: 0 }),
+  max_level: DS.attr('number', { defaultValue: 1 })
 });
