f0929babfba2d437cb248b1f9b27463b110d9928
laniley <melanie.mende@me.com>
2015-07-16 09:54:22 +0200

    google-analytics

--- config/environment.js
+++ config/environment.js
@@ -21,10 +21,10 @@ module.exports = function(environment) {
     // for the FB-SDK
     contentSecurityPolicy: {
       'default-src': "'none'",
-      'script-src': "'self' 'unsafe-inline' 'unsafe-eval' use.typekit.net connect.facebook.net facebook.com graph.facebook.com maps.googleapis.com maps.gstatic.com mt0.googleapis.com mt1.googleapis.com http://www.google-analytics.com/analytics.js",
+      'script-src': "'self' 'unsafe-inline' 'unsafe-eval' www.google-analytics.com use.typekit.net connect.facebook.net facebook.com graph.facebook.com",
       'font-src': "'self' data: use.typekit.net fonts.gstatic.com",
-      'connect-src': "'self' localhost:8000",
-      'img-src': "'self' www.paypalobjects.com graph.facebook.com www.facebook.com p.typekit.net fbcdn-profile-a.akamaihd.net maps.gstatic.com mt0.googleapis.com mt1.googleapis.com maps.googleapis.com csi.gstatic.com mt.googleapis.com",
+      'connect-src': "'self' localhost:8000 connect.facebook.net",
+      'img-src': "'self' www.google-analytics.com www.paypalobjects.com graph.facebook.com www.facebook.com p.typekit.net fbcdn-profile-a.akamaihd.net csi.gstatic.com mt.googleapis.com",
       'style-src': "'self' 'unsafe-inline' use.typekit.net fonts.googleapis.com",
       'frame-src': "s-static.ak.facebook.com static.ak.facebook.com www.facebook.com admin.appnext.com",
       'report-uri': '/_/csp-reports'
