0d573c582def6b2ed356270a00d81d5bc607955d
laniley <melanie.mende@me.com>
2015-07-20 11:10:19 +0200

    syntax fix

--- app/models/rocket.js
+++ app/models/rocket.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   speed: DS.attr('number', { defaultValue: 50 }),
   distanceToGoal: DS.attr('number', { defaultValue: 50 }),
-  distance: DS.attr('numer', { defaultValue: 0 }),
+  distance: DS.attr('number', { defaultValue: 0 }),
   hasACanon: DS.attr('boolean', { defaultValue: false }),
   user: DS.belongsTo('user', { async: true })
 });
