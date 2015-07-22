aa48244ddd66e3976f65b826cbcfb8bf1da6435b
laniley <melanie.mende@me.com>
2015-07-22 11:54:27 +0200

    facebook initializer

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,34 +1,10 @@
-/* global FB */
 import Ember from 'ember';
-import ENV from '../config/environment';
 import FacebookLoginMixin from './../mixins/facebook-login';
 
 export default Ember.Route.extend(FacebookLoginMixin, {
 
   beforeModel: function() {
-
-    var self = this;
-
-    var fbAsyncInit = function() {
-
-      FB.init({
-          appId      : ENV.fb_app_id,
-          xfbml      : true,
-         version    : 'v2.3'
-      });
-
-      self.checkLoginState();
-    };
-
-    (function(d, s, id){
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) {return;}
-        js = d.createElement(s); js.id = id;
-        js.src = "//connect.facebook.net/en_US/sdk.js";
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));
-
-     window.fbAsyncInit = fbAsyncInit;
+    this.checkLoginState();
   },
 
   model: function() {
