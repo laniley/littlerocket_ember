356307abb5170d644a5c3ae0637e0960c011e0c1
laniley <melanie.mende@me.com>
2015-08-02 19:01:53 +0200

    change to unminified version of quintus for better debugging

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -22,7 +22,7 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
-  app.import('vendor/quintus/dist/quintus-all.min.js');
+  app.import('vendor/quintus/dist/quintus-all.js');
   app.import('vendor/jquery/jquery.pietimer.min.js');
 
   return app.toTree();
