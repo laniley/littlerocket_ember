602994c978c80f19fbea47c4133bdf226436f570
laniley <melanie.mende@me.com>
2015-08-05 16:27:18 +0200

    prompt-component

---
+++ app/components/prompt-component.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  didInsertElement: function() {
+    setTimeout(() => {
+      this.set('targetObject.show_not_enough_stars_alert', false);
+    }, 3000);
+  }
+});
