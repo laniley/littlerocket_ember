ba9ec083635d59f90299bc407c3171d897e9da22
laniley <melanie.mende@me.com>
2015-08-08 15:56:24 +0200

    highlight selected upgrade-model

--- app/components/component-upgrades.js
+++ app/components/component-upgrades.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import DS from 'ember-data';
 
 export default Ember.Component.extend({
   componentType: '',
@@ -12,21 +13,45 @@ export default Ember.Component.extend({
       return store.query('canon-model-mm', {
         'canon': this.get('component').get('id')
       }).then(models => {
-        return models
+        return models;
       });
     }
     else if(this.get('componentType') === 'shield') {
       return store.query('shield-model-mm', {
         'shield': this.get('component').get('id')
       }).then(models => {
-        return models
+        return models;
       });
     }
     else if(this.get('componentType') === 'engine') {
       return store.query('engine-model-mm', {
         'engine': this.get('component').get('id')
       }).then(models => {
-        return models
+        return models;
+      });
+    }
+  }.property(),
+
+  selectedModel: function() {
+    if(this.get('componentType') === 'canon') {
+      return this.get('component').get('selectedCanonModelMm').then(selectedCanonModelMm => {
+        return selectedCanonModelMm.get('canonModel').then(canonModel => {
+          return canonModel;
+        });
+      });
+    }
+    else if(this.get('componentType') === 'shield') {
+      return this.get('component').get('selectedShieldModelMm').then(selectedShieldModelMm => {
+        return selectedShieldModelMm.get('shieldModel').then(shieldModel => {
+          return shieldModel;
+        });
+      });
+    }
+    else if(this.get('componentType') === 'engine') {
+      return this.get('component').get('selectedEngineModelMm').then(selectedEngineModelMm => {
+        return selectedEngineModelMm.get('shieldModel').then(engineModel => {
+          return engineModel;
+        });
       });
     }
   }.property(),
@@ -39,21 +64,30 @@ export default Ember.Component.extend({
       return DS.PromiseArray.create({
         promise: store.findAll('canon-model').then(models => {
           return this.get('myModels').then(myModels => {
-            var array = [];
-            models.forEach(function(aModel) {
-              var status = 'locked';
-              myModels.forEach(function(aMyModel) {
-                if(aMyModel.get('canonModel').get('id') === aModel.get('id')) {
-                  status = aMyModel.get('status');
+            return this.get('selectedModel').then(selectedModel => {
+              var array = [];
+              models.forEach(function(aModel) {
+                var status = 'locked';
+                myModels.forEach(function(aMyModel) {
+                  if(aMyModel.get('canonModel').get('id') === aModel.get('id')) {
+                    status = aMyModel.get('status');
+                  }
+                });
+                var isSelected = false;
+
+                if(aModel.get('id') === selectedModel.get('id')) {
+                  isSelected = true;
+                  console.log(selectedModel.get('id'), 'true');
                 }
+                var model = {
+                  model: aModel.get('model'),
+                  status: status,
+                  isSelected: isSelected
+                };
+                array.push(model);
               });
-              var model = {
-                model: aModel.get('model'),
-                status: status
-              };
-              array.push(model);
+              return array;
             });
-            return array;
           });
         })
       });
