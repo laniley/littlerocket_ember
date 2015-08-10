382c23959811a7f34ca869a901d4590864cb3cf1
laniley <melanie.mende@me.com>
2015-08-10 21:56:13 +0200

    reworked

--- app/components/component-upgrades.js
+++ app/components/component-upgrades.js
@@ -5,157 +5,59 @@ export default Ember.Component.extend({
   componentType: '',
   component: null,
 
-  myModels: function() {
-
+  componentModels: function() {
     var store = this.get('targetObject.store');
 
-    if(this.get('componentType') === 'canon') {
-      return store.query('canon-model-mm', {
-        'canon': this.get('component').get('id')
-      }).then(models => {
-        return models;
-      });
-    }
-    else if(this.get('componentType') === 'shield') {
-      return store.query('shield-model-mm', {
-        'shield': this.get('component').get('id')
-      }).then(models => {
-        return models;
-      });
-    }
-    else if(this.get('componentType') === 'engine') {
-      return store.query('engine-model-mm', {
-        'engine': this.get('component').get('id')
-      }).then(models => {
-        return models;
-      });
-    }
+    return store.query('rocket-component-model-mm', {
+      'rocketComponent': this.get('component').get('id')
+    }).then(models => {
+      return models;
+    });
   }.property(),
 
   selectedModel: function() {
-    if(this.get('componentType') === 'canon') {
-      return this.get('component').get('selectedCanonModelMm').then(selectedCanonModelMm => {
-        return selectedCanonModelMm.get('canonModel').then(canonModel => {
-          return canonModel;
-        });
+    return this.get('component').get('selectedRocketComponentModelMm').then(selectedRocketComponentModelMm => {
+      return selectedRocketComponentModelMm.get('rocketComponentModel').then(rocketComponentModel => {
+        return rocketComponentModel;
       });
-    }
-    else if(this.get('componentType') === 'shield') {
-      return this.get('component').get('selectedShieldModelMm').then(selectedShieldModelMm => {
-        return selectedShieldModelMm.get('shieldModel').then(shieldModel => {
-          return shieldModel;
-        });
-      });
-    }
-    else if(this.get('componentType') === 'engine') {
-      return this.get('component').get('selectedEngineModelMm').then(selectedEngineModelMm => {
-        return selectedEngineModelMm.get('shieldModel').then(engineModel => {
-          return engineModel;
-        });
-      });
-    }
+    });
   }.property(),
 
   models: function() {
 
     var store = this.get('targetObject.store');
 
-    if(this.get('componentType') === 'canon') {
-      return DS.PromiseArray.create({
-        promise: store.findAll('canon-model').then(models => {
-          return this.get('myModels').then(myModels => {
-            return this.get('selectedModel').then(selectedModel => {
-              var array = [];
-              models.forEach(function(aModel) {
-                var status = 'locked';
-                myModels.forEach(function(aMyModel) {
-                  if(aMyModel.get('canonModel').get('id') === aModel.get('id')) {
-                    status = aMyModel.get('status');
-                  }
-                });
-                var isSelected = false;
-                if(aModel.get('id') === selectedModel.get('id')) {
-                  isSelected = true;
+    return DS.PromiseArray.create({
+      promise: store.query('rocket-component-model', { 'type': this.get('componentType') }).then(models => {
+        return this.get('componentModels').then(componentModels => {
+          return this.get('selectedModel').then(selectedModel => {
+            var array = [];
+            models.forEach(function(aModel) {
+              var status = 'locked';
+              componentModels.forEach(function(aComponentModel) {
+                if(aComponentModel.get('rocketComponentModel').get('id') === aModel.get('id')) {
+                  status = aComponentModel.get('status');
                 }
-                var model = {
-                  model: aModel.get('model'),
-                  costs: aModel.get('costs'),
-                  tooltip: "You need " + aModel.get('costs') + " stars to unlock this canon model.",
-                  status: status,
-                  isSelected: isSelected
-                };
-                array.push(model);
               });
-              return array;
+              var isSelected = false;
+              if(aModel.get('id') === selectedModel.get('id')) {
+                isSelected = true;
+              }
+              var model = {
+                model: aModel.get('model'),
+                costs: aModel.get('costs'),
+                tooltip: "You need " + aModel.get('costs') + " stars to unlock this canon model.",
+                status: status,
+                isSelected: isSelected
+              };
+              array.push(model);
             });
+            return array;
           });
-        })
-      });
-    }
-    else if(this.get('componentType') === 'shield') {
-      return DS.PromiseArray.create({
-        promise: store.findAll('shield-model').then(models => {
-          return this.get('myModels').then(myModels => {
-            return this.get('selectedModel').then(selectedModel => {
-              var array = [];
-              models.forEach(function(aModel) {
-                var status = 'locked';
-                myModels.forEach(function(aMyModel) {
-                  if(aMyModel.get('shieldModel').get('id') === aModel.get('id')) {
-                    status = aMyModel.get('status');
-                  }
-                });
-                var isSelected = false;
-                if(aModel.get('id') === selectedModel.get('id')) {
-                  isSelected = true;
-                }
-                var model = {
-                  model: aModel.get('model'),
-                  costs: aModel.get('costs'),
-                  tooltip: "You need " + aModel.get('costs') + " stars to unlock this shield model.",
-                  status: status,
-                  isSelected: isSelected
-                };
-                array.push(model);
-              });
-              return array;
-            });
-          });
-        })
-      });
-    }
-    else if(this.get('componentType') === 'engine') {
-      return DS.PromiseArray.create({
-        promise: store.findAll('engine-model').then(models => {
-          return this.get('myModels').then(myModels => {
-            return this.get('selectedModel').then(selectedModel => {
-              var array = [];
-              models.forEach(function(aModel) {
-                var status = 'locked';
-                myModels.forEach(function(aMyModel) {
-                  if(aMyModel.get('engineModel').get('id') === aModel.get('id')) {
-                    status = aMyModel.get('status');
-                  }
-                });
-                var isSelected = false;
-                if(aModel.get('id') === selectedModel.get('id')) {
-                  isSelected = true;
-                }
-                var model = {
-                  model: aModel.get('model'),
-                  costs: aModel.get('costs'),
-                  tooltip: "You need " + aModel.get('costs') + " stars to unlock this engine model.",
-                  status: status,
-                  isSelected: isSelected
-                };
-                array.push(model);
-              });
-              return array;
-            });
-          });
-        })
-      });
-    }
+        });
+      })
+    });
+
   }.property(),
 
   icon: function() {
