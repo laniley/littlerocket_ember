b1cbc83e6dfd4811afeae41b83e6d83b8f92f859
laniley <melanie.mende@me.com>
2015-08-14 22:42:30 +0200

    added keyForRelationship ro serializer

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -1,5 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.RESTSerializer.extend({
-  primaryKey: 'id'
+  primaryKey: 'id',
+  keyForRelationship: function(attr /*, relationshipType, method*/) {
+    return attr + "_id";
+  }
 });
