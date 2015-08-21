bc45b15a32108c973b5d4764cf226d597c63d620
laniley <melanie.mende@me.com>
2015-08-21 17:13:54 +0200

    no revision number for images - quintus can't handle it

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,6 +3,9 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
+    'fingerprint': {
+      'exclude': ['images']
+    },
     'ember-cli-foundation-sass': {
       'modernizr': true,
       'fastclick': true,
