e81fa1896d83e1dbaa84887f018d812915b502d4
laniley <melanie.mende@me.com>
2015-08-21 20:42:22 +0200

    achievements tab added

---
+++ app/components/player-hud-component.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  me: null,
+  currentSection: 'cockpit',
+  currentCockpitSection: 'workbench',
+  currentLabSection: 'canon'
+});
--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,7 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  queryParams: ['cockpit_section', 'lab_section'],
+  queryParams: ['player_hud_section', 'cockpit_section', 'lab_section'],
+  player_hud_section: 'cockpit',
   cockpit_section: 'workbench',
   lab_section: 'canon'
 });
