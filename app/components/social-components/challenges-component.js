3133c61ff32b2d2f7f78dc378c0aa6d53342f87e
laniley <melanie.mende@me.com>
2015-09-23 21:22:58 +0200

    fb ui challenge dialog

--- app/components/social-components/challenges-component.js
+++ app/components/social-components/challenges-component.js
@@ -1,11 +1,17 @@
+/* global FB */
 import Ember from 'ember';
 
 export default Ember.Component.extend({
   me: null,
 
   actions: {
-    sendChallengeRequest: function() {
-      
+    sendChallengeRequest: function(long_fb_id) {
+      FB.ui({method: 'apprequests',
+        message: 'I want to challenge you in Little Rocket! Can you beat me?',
+        to: long_fb_id
+      }, function(response){
+          console.log(response);
+      });
     }
   }
 });
