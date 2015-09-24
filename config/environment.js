f5f9cc90033825092cace9d1d505ca5a5ddb1790
laniley <melanie.mende@me.com>
2015-09-24 20:45:29 +0200

    exchanged appnext with lifestreet

--- config/environment.js
+++ config/environment.js
@@ -21,12 +21,12 @@ module.exports = function(environment) {
     // for the FB-SDK
     contentSecurityPolicy: {
       'default-src': "'none'",
-      'script-src': "'self' 'unsafe-inline' 'unsafe-eval' www.google-analytics.com use.typekit.net connect.facebook.net facebook.com graph.facebook.com",
+      'script-src': "'self' 'unsafe-inline' 'unsafe-eval' cdn.lfstmedia.com ads.lfstmedia.com www.google-analytics.com use.typekit.net connect.facebook.net facebook.com graph.facebook.com",
       'font-src': "'self' data: use.typekit.net fonts.gstatic.com",
       'connect-src': "'self' localhost:8000 littlerocket-game.com connect.facebook.net",
       'img-src': "'self' *.xx.fbcdn.net www.google-analytics.com www.paypalobjects.com fbstatic-a.akamaihd.net graph.facebook.com www.facebook.com p.typekit.net fbcdn-profile-a.akamaihd.net csi.gstatic.com mt.googleapis.com",
       'style-src': "'self' 'unsafe-inline' use.typekit.net fonts.googleapis.com",
-      'frame-src': "s-static.ak.facebook.com static.ak.facebook.com www.facebook.com admin.appnext.com",
+      'frame-src': "ads.lfstmedia.com s-static.ak.facebook.com static.ak.facebook.com www.facebook.com admin.appnext.com",
       'report-uri': '/_/csp-reports'
     }
   };
