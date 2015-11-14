653f81af2e9bc90ca135b6d9d47a6e34d6365e5e
laniley <melanie.mende@me.com>
2015-11-14 20:36:11 +0100

    workbench component created

---
+++ tests/integration/components/player-hud-components/workbench-test.js
@@ -0,0 +1,25 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('player-hud-components/workbench', 'Integration | Component | player hud components/workbench', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });" + EOL + EOL +
+
+  this.render(hbs`{{player-hud-components/workbench}}`);
+
+  assert.equal(this.$().text().trim(), '');
+
+  // Template block usage:" + EOL +
+  this.render(hbs`
+    {{#player-hud-components/workbench}}
+      template block text
+    {{/player-hud-components/workbench}}
+  `);
+
+  assert.equal(this.$().text().trim(), 'template block text');
+});
