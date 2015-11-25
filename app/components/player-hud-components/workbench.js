eb08f786100f7db01f2355029324b774bd3625b3
laniley <melanie.mende@me.com>
2015-11-25 21:15:05 +0100

    renaming

--- app/components/player-hud-components/workbench.js
+++ app/components/player-hud-components/workbench.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
-import ObjectMixin from './../../mixins/object';
+import ObjectMixin from './../../mixins/buyable-object';
 
 export default Ember.Component.extend(ObjectMixin, {
   actions: {
