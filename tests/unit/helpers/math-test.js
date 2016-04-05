763445cf0550409c796d0908e22dbcac0072ab1d
laniley <melanie.mende@me.com>
2016-04-05 09:29:31 +0200

    math-helper

---
+++ tests/unit/helpers/math-test.js
@@ -0,0 +1,10 @@
+import { math } from 'littlerocket/helpers/math';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | math');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = math([42]);
+  assert.ok(result);
+});
