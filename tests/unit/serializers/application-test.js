eb62e38a1fa7086d93ee38b8b327983e6e402f7c
laniley <melanie.mende@me.com>
2016-04-16 17:55:39 +0200

    test for serializer

--- tests/unit/serializers/application-test.js
+++ tests/unit/serializers/application-test.js
@@ -1,15 +1,42 @@
 import { moduleFor, test } from 'ember-qunit';
+import Pretender from 'pretender';
+import Ember from 'ember';
+
+var server;
 
 moduleFor('serializer:application', 'Unit | Serializer | application', {
   // Specify the other units that are required for this test.
-  needs: ['serializer:application']
+  needs: ['serializer:application', 'model:user', 'model:lab'],
+  beforeEach() {
+    server = new Pretender(function() {
+      this.get('/users', function() {
+        var users = { "users": [
+            { "id": 1, "first_name": "Melanie", "lab_id": 1 }
+        ]};
+        return [200, { "Content-Type": "application/json" }, JSON.stringify(users)];
+      });
+    });
+  },
+  afterEach() {
+    server.shutdown();
+  }
 });
 
-// Replace this with your real tests.
-test('it serializes records', function(assert) {
-  var record = this.subject();
+test('belongsTo relationship uses foreign keys in the format XXX_id', function(assert) {
 
-  var serializedRecord = record.serialize();
+  let store = this.container.lookup('service:store');
 
-  assert.ok(serializedRecord);
+  Ember.run(() => {
+    store.push({
+      data: {
+        id: '1',
+        type: 'lab',
+        attributes: {}
+      }
+    });
+  });
+
+  return store.findAll('user').then((users) => {
+    assert.equal(users.objectAt(0).get('lab.id'), '1');
+  });
 });
