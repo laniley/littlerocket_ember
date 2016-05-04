8cfaf5610d131f6a5c64e77dc973aeec62c4e959
laniley <melanie.mende@me.com>
2016-05-04 23:51:18 +0200

    moved prompt-component to lib folder

--- tests/integration/components/lib/prompt-component-test.js
+++ tests/integration/components/lib/prompt-component-test.js
@@ -12,15 +12,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{prompt-component}}`);
+  this.render(hbs`{{lib/prompt-component}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#prompt-component}}
+    {{#lib/prompt-component}}
       template block text
-    {{/prompt-component}}
+    {{/lib/prompt-component}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
