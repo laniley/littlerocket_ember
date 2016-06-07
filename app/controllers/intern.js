1eb589e5598c2ba257025cd165b14d8c15526197
laniley <melanie.mende@me.com>
2016-06-07 22:25:32 +0200

    added route intern

---
+++ app/controllers/intern.js
@@ -0,0 +1,28 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+
+    queryParams: [
+      'overlay_section',
+      'player_hud_section',
+        'cockpit_section',
+        'lab_section',
+      'social_section',
+        'challenges_section',
+        'leaderboard_filterBy',
+        'leaderboard_sortBy',
+        'armada_section',
+        'armada_main_section'
+    ],
+    overlay_section: 'none',
+    player_hud_section: 'rocket',
+      cockpit_section: 'workbench',
+      lab_section: 'cannon',
+    social_section: 'leaderboard',
+      challenges_section: 'unplayed',
+      leaderboard_filterBy: 'all',
+      leaderboard_sortBy: 'score',
+      armada_section: 'main',
+      armada_main_section: 'home',
+
+});
