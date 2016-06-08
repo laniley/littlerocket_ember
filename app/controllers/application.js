1412d632988b88505da6bed2a520eb23c1bad557
laniley <melanie.mende@me.com>
2016-06-08 18:04:04 +0200

    intern route added

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,31 +2,7 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  queryParams: [
-    'overlay_section',
-    'player_hud_section',
-      'cockpit_section',
-      'lab_section',
-    'social_section',
-      'challenges_section',
-      'leaderboard_filterBy',
-      'leaderboard_sortBy',
-      'armada_section',
-      'armada_main_section'
-  ],
-  overlay_section: 'none',
-  player_hud_section: 'rocket',
-    cockpit_section: 'workbench',
-    lab_section: 'cannon',
-  social_section: 'leaderboard',
-    challenges_section: 'unplayed',
-    leaderboard_filterBy: 'all',
-    leaderboard_sortBy: 'score',
-    armada_section: 'main',
-    armada_main_section: 'home',
-
   didRender() {
-    console.log('TEST');
     new LSM_Slot({
         adkey: '6df',
         ad_size: '728x90',
