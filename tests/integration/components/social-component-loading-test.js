d91c3b8ccfec2cec5c5c1628381f80755a3f2ef7
laniley <melanie.mende@me.com>
2016-06-18 09:50:12 +0200

    fix for loading screen issue when already logged in

---
+++ tests/integration/components/social-component-loading-test.js
@@ -0,0 +1,14 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('social-component-loading', 'Integration | Component | social component loading', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });"
+
+  this.render(hbs`{{social-component-loading}}`);
+  assert.equal(this.$().html().length > 0, true);
+});
