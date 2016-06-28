6bd83b0582682ff1e054a033ac9dca2b2ab9ec1a
laniley <melanie.mende@me.com>
2016-06-28 15:29:16 +0200

    moved the ad-banner from the footer section in the app template to the pop-ups. so it is more prominent to the user and it does no longer effect the game performance.

--- app/components/ad-banner.js
+++ app/components/ad-banner.js
@@ -2,13 +2,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+  classNames: ['ad-banner'],
+
   didInsertElement() {
     try{
       new LSM_Slot({
           adkey: '6df',
           ad_size: '728x90',
           slot: 'slot126743',
-          _render_div_id: 'footer_banner',
+          _render_div_id: this.get('id'),
           _preload: true
       });
     }
--- tests/integration/components/ad-banner-test.js
+++ tests/integration/components/ad-banner-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('footer-banner', 'Integration | Component | footer banner', {
+moduleForComponent('ad-banner', 'Integration | Component | ad banner', {
   integration: true
 });
 
@@ -9,15 +9,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });"
 
-  this.render(hbs`{{footer-banner}}`);
+  this.render(hbs`{{ad-banner}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:"
   this.render(hbs`
-    {{#footer-banner}}
+    {{#ad-banner}}
       template block text
-    {{/footer-banner}}
+    {{/ad-banner}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
