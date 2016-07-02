d82af9764b13a9754aa64480c2d7c8241f059c38
chrizzdf <tools.christoph.langhof@gmail.com>
2016-07-02 05:13:13 +0200

    - replace old progress-bar component with a new universal one - refactor progress styles

---
+++ app/components/lr-progress.js
@@ -0,0 +1,53 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+
+  classNames: ['lr-progress'],
+
+  reached: 0,
+
+  total: 100,
+
+  withAchievementPoints: false,
+
+  percentage: Ember.computed('reached', 'total', function () {
+
+    return this.get('reached') / this.get('total') * 100;
+
+  }),
+
+  isCompleted: Ember.computed('percentage', function () {
+
+    if (Number(this.get('percentage')) === 100) {
+
+      return true;
+
+    } else {
+
+      return false;
+
+    }
+
+  }),
+
+  inProgress: Ember.computed('percentage', function () {
+
+    if (Number(this.get('percentage')) < 100 && Number(this.get('percentage')) > 0) {
+
+      return true;
+
+    } else {
+
+      return false;
+
+    }
+
+  }),
+
+  barWidth: Ember.computed('percentage', function () {
+
+    return new Ember.Handlebars.SafeString('width:' + this.get('percentage') + '%');
+
+  }),
+
+});
---
+++ tests/integration/components/lr-progress-test.js
@@ -0,0 +1,24 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('lr-progress', 'Integration | Component | lr progress', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  this.render(hbs`{{lr-progress}}`);
+
+  assert.equal(this.$().text().trim(), '');
+
+  // Template block usage:
+  this.render(hbs`
+    {{#lr-progress}}
+      template block text
+    {{/lr-progress}}
+  `);
+
+  assert.equal(this.$().text().trim(), 'template block text');
+});
