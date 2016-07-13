269c7494f01c985861686bb0ff8c3eac4281ce89
laniley <melanie.mende@me.com>
2016-07-13 20:09:18 +0200

    clean-up for deployment

--- tests/integration/components/lib/lr-progress-test.js
+++ tests/integration/components/lib/lr-progress-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('lr-progress', 'Integration | Component | lr progress', {
+moduleForComponent('lib/lr-progress', 'Integration | Component | lr progress', {
   integration: true,
 });
 
@@ -9,16 +9,16 @@ test('it renders', function (assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{lr-progress}}`);
+  this.render(hbs`{{lib/lr-progress}}`);
 
-  assert.equal(this.$().text().trim(), '');
+  assert.equal(this.$().text().trim(), '0/100');
 
   // Template block usage:
   this.render(hbs`
-    {{#lr-progress reached=0}}
+    {{#lib/lr-progress reached=0}}
       template block text
-    {{/lr-progress}}
+    {{/lib/lr-progress}}
   `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$().text().trim(), '0/100');
 });
