81cdf3e5ce5ee5b78cfed44d6dedaac1223f3687
laniley <melanie.mende@me.com>
2016-07-13 19:29:15 +0200

    margin-bottom optional

--- app/components/lr-progress.js
+++ app/components/lr-progress.js
@@ -4,52 +4,38 @@ export default Ember.Component.extend({
 
   classNames: ['lr-progress'],
 
-  classNameBindings: ['isCompleted:lr-progress--is-completed', 'success:lr-progress--success'],
+  classNameBindings: [
+    'isCompleted:lr-progress--is-completed',
+    'success:lr-progress--success',
+    'withAchievementPoints:lr-progress--with-margin-bottom'
+  ],
 
   reached: 0,
-
   total: 100,
-
   withAchievementPoints: false,
 
   percentage: Ember.computed('reached', 'total', function () {
-
     return this.get('reached') / this.get('total') * 100;
-
   }),
 
   isCompleted: Ember.computed('percentage', function () {
-
     if (Number(this.get('percentage')) === 100) {
-
       return true;
-
     } else {
-
       return false;
-
     }
-
   }),
 
   inProgress: Ember.computed('percentage', function () {
-
     if (Number(this.get('percentage')) < 100 && Number(this.get('percentage')) > 0) {
-
       return true;
-
     } else {
-
       return false;
-
     }
-
   }),
 
   barWidth: Ember.computed('percentage', function () {
-
     return new Ember.Handlebars.SafeString('width:' + this.get('percentage') + '%');
-
   }),
 
 });
