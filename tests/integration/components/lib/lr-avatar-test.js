2a35f1bfb49b246c36ada161435d4a41a4efc373
laniley <melanie.mende@me.com>
2016-07-16 12:21:26 +0200

    fixed test

--- tests/integration/components/lib/lr-avatar-test.js
+++ tests/integration/components/lib/lr-avatar-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('lr-avatar', 'Integration | Component | lr avatar', {
+moduleForComponent('lib/lr-avatar', 'Integration | Component | lr avatar', {
   integration: true
 });
 
@@ -9,15 +9,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{lr-avatar}}`);
+  this.render(hbs`{{lib/lr-avatar}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#lr-avatar}}
+    {{#lib/lr-avatar}}
       template block text
-    {{/lr-avatar}}
+    {{/lib/lr-avatar}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
