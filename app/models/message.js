9f4b26305cadcc92d0a80009ccf1486cb6bd9b93
laniley <melanie.mende@me.com>
2016-08-05 08:05:43 +0200

    clean up messages model

--- app/models/message.js
+++ app/models/message.js
@@ -1,9 +1,11 @@
-import DS from 'ember-data';
+import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
+import { belongsTo /*, hasMany*/ } from 'ember-data/relationships';
 
-export default DS.Model.extend({
-  from_user: DS.belongsTo('user', { inverse: 'messages_send' }),
-  to_user: DS.belongsTo('user', { inverse: 'messages_received' }),
-  type: DS.attr('string'),
+export default Model.extend({
+  from_user: belongsTo('user', { inverse: 'messages_send' }),
+  to_user: belongsTo('user', { inverse: 'messages_received' }),
+  type: attr('string'),
 
   delete() {
     this.destroyRecord();
