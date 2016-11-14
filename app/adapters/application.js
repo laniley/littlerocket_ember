6a67d8c8169a858662293e3f74e52f1521976e6a
laniley <melanie.mende@me.com>
2016-11-14 11:33:31 +0100

    bearer token authorization

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,9 @@
 import DS from 'ember-data';
+import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 import ENV from '../config/environment';
 
-export default DS.RESTAdapter.extend({
-  host: ENV.backend_url,
-  namespace: ENV.backend_namespace
+export default DS.RESTAdapter.extend(DataAdapterMixin, {
+    host: ENV.backend_url,
+    namespace: ENV.backend_namespace,
+    authorizer: 'authorizer:token',
 });
