e60b516d828aaf2f5f0b9d380b16865c846f5221
laniley <melanie.mende@me.com>
2017-02-07 22:06:43 +0100

    new vertical ad-banners

--- app/components/ad-banner.js
+++ app/components/ad-banner.js
@@ -2,21 +2,24 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    // web worker example https://github.com/remy/html5demos/blob/master/js/worker-main.js
+    classNames: ['ad-banner'],
+    classNameBindings: ['type'],
+    elementId: 'ad-banner-footer',
 
-  classNames: ['ad-banner'],
-
-  didInsertElement() {
-    try{
-      new LSM_Slot({
-          adkey: '6df',
-          ad_size: '728x90',
-          slot: 'slot126743',
-          _render_div_id: this.get('id'),
-          _preload: true
-      });
+    didInsertElement() {
+        try{
+            new LSM_Slot({
+                adkey: this.get('adKey'),
+                ad_size: this.get('adSize'),
+                slot: this.get('slot'),
+                _render_div_id: this.get('elementId'),
+                _preload: true,
+                // _guard: true
+            });
+        }
+        catch(e) {
+            console.log(e);
+        }
     }
-    catch(e) {
-      console.log(e);
-    }
-  }
 });
--- app/initializers/lfstmedia.js
+++ app/initializers/lfstmedia.js
@@ -1,24 +1,7 @@
-/* global LSM_Slot */
 import injectScript from 'ember-inject-script';
 
 export function initialize(/* application */) {
-    var url = "//ads.lfstmedia.com/getad?site=268210";
-    injectScript(url).then(function() {
-        new LSM_Slot({
-            adkey: '6df',
-            ad_size: '728x90',
-            slot: 'slot126743',
-            _render_div_id: 'footer_banner',
-            _preload: true
-        });
-        new LSM_Slot({
-            adkey: 'd93',
-            ad_size: '160x600',
-            slot: 'slot168551',
-            _render_div_id: 'left_side_banner',
-            _preload: true
-        });
-    });
+    injectScript("//ads.lfstmedia.com/getad?site=268210");
 }
 
 export default {
