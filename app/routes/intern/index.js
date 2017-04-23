577f859b40d40c2abaecc835a90f156a3156dd88
laniley <melanie.mende@me.com>
2017-04-23 21:50:22 +0200

    auto redirect ro main-menu

--- app/routes/intern/index.js
+++ app/routes/intern/index.js
@@ -1,3 +1,7 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({});
+export default Ember.Route.extend({
+	beforeModel() {
+        this.transitionTo('intern.main-menu');
+    },
+});
